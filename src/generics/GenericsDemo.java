22da04d14d47b2b5aac816d3c389dc9a0cffc077
Zach Ayers <ayers.dev@icloud.com>
2021-02-20 23:21:05 -0600

    add generic demo

---
+++ src/generics/GenericsDemo.java
@@ -0,0 +1,12 @@
+package generics;
+
+import generics.implementations.Bucket;
+
+public class GenericsDemo {
+    public static void main(String[] args) {
+
+        var bucket = new Bucket();
+
+        bucket.add(1);
+    }
+}
---
+++ src/generics/implementations/Bucket.java
@@ -0,0 +1,14 @@
+package generics.implementations;
+
+public class Bucket<T> {
+    private final T[] items = (T[]) new Object[10];
+    private int count;
+
+    public void add(T item) {
+        items[count++] = item;
+    }
+
+    public T get(int index) {
+        return items[index];
+    }
+}
